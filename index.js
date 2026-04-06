9aa5ecc3476f19c80ef8c8d09affe1dc46d0992c
Nadeem Khan <nadeem5278652@gmail.com>
2026-04-06 22:54:11 +0530

    add supabase

--- index.js
+++ index.js
@@ -38,11 +38,10 @@ app.use((req, res, next) => {
 });
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();

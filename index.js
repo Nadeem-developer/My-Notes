581c3ddf051ac46f0d0df4374bd81e114ef672cd
Nadeem Khan <nadeem5278652@gmail.com>
2026-04-07 00:00:50 +0530

    not works... test

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 3000;
 const saltRounds = 15;
-const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
